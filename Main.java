4538bd7aa999b9aa879a6f09edad3698c34d522d
Arthigasri <arthi1461997@gmail.com>
2017-07-04 17:52:00 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+# fss
+import java.io.*;
+import java.util.*;
+public class Main
+{
+Scanner sc=new Scanner(System.in)
+int a;
+if(a>0)
+{
+System.out.println("positive");
+}
+else
+{
+System.out.print("Negative");
+}
+}
+}
+
